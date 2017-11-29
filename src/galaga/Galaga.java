c2bd89a721e74fed4f1cd7c096d43d826d1eb461
cn20kaman <34107543+cn20kaman@users.noreply.github.com>
2017-11-29 15:22:22 -0500

    Update Galaga.java

--- src/galaga/Galaga.java
+++ src/galaga/Galaga.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package galaga;
 
-/**
- *
- * @author bspada
- */
 public class Galaga {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        JFrame frame = new JFrame ("Game");
     }
  
-    
-    //hello
 }
