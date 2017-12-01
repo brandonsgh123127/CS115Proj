3afc1d616e8ac021794652bc290fbf944853c03a
SamPLO01 <34107852+SamPLO01@users.noreply.github.com>
2017-11-30 21:08:59 -0500

    Add files via upload

--- src/galaga/Controlled_Player.java
+++ src/galaga/Controlled_Player.java
@@ -21,45 +21,106 @@ public class Controlled_Player extends Player{
     }
     
     /**
-     * Paints the players space ship
+     * Paints the players space ship pixel by pixel
      * @param g the graphics
      */
     @Override
     public void paint(Graphics g){
         g.setColor(Color.WHITE);
         //Line 1
-        g.fillRect(getXPos() - getWidth()*7, getYPos(), getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight(), getWidth(), getHeight());
         
         //Line 2
-        g.fillRect(getXPos() - getWidth()*7, getYPos(), getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*2, getWidth(), getHeight());
         
         //Line 3
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*3, getWidth(), getHeight());
         
         //Line 4
+        g.setColor(Color.RED);
+        g.fillRect(getXPos() + getWidth()*3, getYPos() + getHeight()*4, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*9, getYPos() + getHeight()*4, getWidth(), getHeight());
         
+        g.setColor(Color.WHITE);
+        g.fillRect(getXPos() + getWidth()*5, getYPos() + getHeight()*4, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*4, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*7, getYPos() + getHeight()*4, getWidth(), getHeight());
+ 
         //Line 5
+        g.fillRect(getXPos() + getWidth()*3, getYPos() + getHeight()*5, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*9, getYPos() + getHeight()*5, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*5, getYPos() + getHeight()*5, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*5, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*7, getYPos() + getHeight()*5, getWidth(), getHeight());
         
         //Line 6
+        g.setColor(Color.BLUE);
+        g.fillRect(getXPos() + getWidth()*4, getYPos() + getHeight()*6, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*8, getYPos() + getHeight()*6, getWidth(), getHeight());
+        
+        
+        g.setColor(Color.RED);
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*6, getWidth(), getHeight());
+        
+        g.setColor(Color.WHITE);
+        g.fillRect(getXPos() + getWidth()*3, getYPos() + getHeight()*6, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*5, getYPos() + getHeight()*6, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*7, getYPos() + getHeight()*6, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*9, getYPos() + getHeight()*6, getWidth(), getHeight());
         
         //Line 7
+        g.setColor(Color.BLUE);
+        g.fillRect(getXPos() + getWidth()*3, getYPos() + getHeight()*7, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*9, getYPos() + getHeight()*7, getWidth(), getHeight());
         
-        //Line 7
+        g.setColor(Color.RED);
+        g.fillRect(getXPos() + getWidth(), getYPos() + getHeight()*7, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*5, getYPos() + getHeight()*7, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*7, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*7, getYPos() + getHeight()*7, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*11, getYPos() + getHeight()*7, getWidth(), getHeight());
+        
+        g.setColor(Color.WHITE);
+        g.fillRect(getXPos() + getWidth()*4, getYPos() + getHeight()*7, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*8, getYPos() + getHeight()*7, getWidth(), getHeight());
         
         //Line 8
+        g.setColor(Color.RED);
+        g.fillRect(getXPos() + getWidth()*5, getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*7, getYPos() + getHeight()*8, getWidth(), getHeight());
+        
+        g.setColor(Color.WHITE);
+        g.fillRect(getXPos() + getWidth(), getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*3, getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*4, getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*8, getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*9, getYPos() + getHeight()*8, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*11, getYPos() + getHeight()*8, getWidth(), getHeight());
         
         //Line 9
+        for(int i = 1; i < 12; i++){
+            g.fillRect(getXPos() + getWidth()*i, getYPos() + getHeight()*9, getWidth(), getHeight());
         
+        }
         //Line 10
+        g.setColor(Color.RED);
+        g.fillRect(getXPos() + getWidth()*4, getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*5, getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*7, getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*8, getYPos() + getHeight()*10, getWidth(), getHeight());
+        
+        g.setColor(Color.WHITE);
+        g.fillRect(getXPos() + getWidth(), getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*2, getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*10, getYPos() + getHeight()*10, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*11, getYPos() + getHeight()*10, getWidth(), getHeight());
         
         //Line 11
-        
-        //Line 12
-        
-        //Line 13
-        
-        //Line 14
-        
-        //Line 15
+        g.fillRect(getXPos() + getWidth(), getYPos() + getHeight()*11, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*6, getYPos() + getHeight()*11, getWidth(), getHeight());
+        g.fillRect(getXPos() + getWidth()*11, getYPos() + getHeight()*11, getWidth(), getHeight());
     }
     
 }
