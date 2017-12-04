9fa2d1b783c6b1df99a6949d393765ca47f388ae
cn20kaman <34107543+cn20kaman@users.noreply.github.com>
2017-12-04 07:21:37 -0500

    Add files via upload

---
+++ src/galaga/Game_Object.java
@@ -0,0 +1,86 @@
+
+package galaga;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Game_Object 
+{
+    private int x_pos;
+    private int y_pos;
+    private int hieght;
+    private int width;
+    private int x_move;
+    private int y_move;
+    private Color color;
+
+    public Game_Object(int x_pos, int y_pos, int hieght, int width, int x_move, int y_move)
+    {
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+        this.hieght = hieght;
+        this.width = width;
+        this.x_move = x_move;
+        this.y_move = y_move;
+    }
+
+    public int get_xpos() { return x_pos;}
+    public void set_xpos(int x_pos) { this.x_pos = x_pos;}
+
+    public int get_ypos() { return y_pos;}
+    public void set_ypos(int y_pos) { this.y_pos = y_pos;}
+
+    public int get_hieght() { return hieght;}
+    public void set_hieght(int hieght) { this.hieght = hieght;}
+
+    public int get_width() { return width;}
+    public void set_width(int width) { this.width = width;}
+
+    public int get_xmove() { return x_move;}
+    public void set_xmove(int x_move) { this.x_move = x_move;}
+
+    public int get_ymove() { return y_move;}
+    public void set_ymove(int y_move) { this.y_move = y_move;}
+
+    public Color get_color() { return color;}
+    public void set_color(Color color) { this.color = color;}
+    
+    public void set_pos(int x, int y)
+    {
+        this.x_pos = x;
+        this.y_pos = y;
+    }
+    
+    public void move_up()
+    {
+        y_pos -= y_move;
+    }
+    public void move_down()
+    {
+        y_pos += y_move;
+    }
+    public void move_right()
+    {
+        x_pos += x_move;
+    }
+    public void move_left()
+    {
+        x_pos -= x_move;
+    }
+    
+    public void paint(Graphics g)
+    {
+        g.fillRect(x_pos, y_pos, width, hieght);
+    }
+    
+    public boolean hit(Game_Object o)
+    {
+        return true;
+    }
+    
+    public boolean out_of_bounds(int screen_width, int screen_hieght)
+    {
+        return (x_pos > screen_width || y_pos > screen_hieght || x_pos + width < 0 || y_pos + hieght < 0);
+    }
+    
+}
