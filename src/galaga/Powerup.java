a195218e71c1428c172e89d14582e347c2a36981
Samuel <Samuel@192.168.0.11>
2017-12-08 22:07:52 -0500

    Changed powerup so it doesn't extend gameobject

--- src/galaga/Powerup.java
+++ src/galaga/Powerup.java
@@ -4,6 +4,14 @@ package galaga;
  *
  * @author Samuel
  */
-public class Powerup extends Game_Object{
+public class Powerup{
+    private final int WIDTH, HEIGHT;
+    
+    
+    public Powerup(int WIDTH, int HEIGHT) {
+        this.WIDTH = WIDTH;
+        this.HEIGHT = HEIGHT;
+    }
+    
     
 }
